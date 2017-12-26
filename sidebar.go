bccf072c903f4df328e0ecf549cc2bc56db42370
Camille Scholtz <camille@airmail.cc>
2017-12-26 17:16:04 +0100

    Add particle.js background to card

--- sidebar.go
+++ sidebar.go
@@ -4,7 +4,9 @@ import (
 	"os"
 	"path"
 
-	qrcode "github.com/skip2/go-qrcode"
+	"image/color"
+
+	qrcode "github.com/onodera-punpun/go-qrcode"
 )
 
 // Sidebar is a stuct with all sidebar values.
@@ -12,12 +14,14 @@ type Sidebar struct {
 	Balance   float64
 	UnBalance float64
 	Address   string
+	Height    int64
 }
 
 func sidebar() (Sidebar, error) {
 	s := Sidebar{}
 
 	// Get wallet balance.
+	// TODO: Make this always use the same amount of decimals.
 	b, err := wallet.GetBalance()
 	if err != nil {
 		return s, err
@@ -29,11 +33,17 @@ func sidebar() (Sidebar, error) {
 		return s, err
 	}
 
+	// Get the wallet's current block height.
+	h, err := wallet.GetHeight()
+	if err != nil {
+		return s, err
+	}
+
 	// Generate QR image.
 	if _, err := os.Stat(path.Join("static/images/qr", a.Address+
 		".png")); os.IsNotExist(err) {
-		if err := qrcode.WriteFile(a.Address, qrcode.Medium, 175, path.Join(
-			"static/images/qr", a.Address+".png")); err != nil {
+		if err := qrcode.WriteColorFile(a.Address, qrcode.Medium, 226, color.Transparent, color.White,
+			path.Join("static/images/qr", a.Address+".png")); err != nil {
 			return s, err
 		}
 	}
@@ -41,6 +51,7 @@ func sidebar() (Sidebar, error) {
 	s.Balance = float64(b.Balance) / 1.e+12
 	s.UnBalance = float64(b.UnBalance) / 1.e+12
 	s.Address = a.Address
+	s.Height = h.Height
 
 	return s, nil
 }
